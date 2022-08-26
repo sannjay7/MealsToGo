f885ac4f373b33f01a29d95018498145bee72538
sannjay7 <sannjay7@gmail.com>
2022-08-26 19:26:37 +0530

    SatusBar

--- App.js
+++ App.js
@@ -1,19 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StatusBar,StyleSheet, Text, View,SafeAreaView,Platform } from 'react-native';
 
+const STATUSBAR_HEIGHT = Platform.OS === 'android' ? 30 : StatusBarManager.HEIGHT;
 export default function App() {
   return (
+    <SafeAreaView style={{ flex: 1, marginTop: STATUSBAR_HEIGHT }}>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Search</Text>
+      <Text>Lets Start</Text>
       <StatusBar style="auto" />
     </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    padding: 16,
+    backgroundColor: '#F6C2FF',
     alignItems: 'center',
     justifyContent: 'center',
   },
